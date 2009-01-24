1ba432314951327e4c2412661298273dacc94144
Dima Lunich <lunich@ukr.net>
2009-01-24 02:17:15 +0200

    players and tournaments links updated

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,8 @@ class PlayersController < ApplicationController
   def index
     @players = Player.all_active
   end
+
+  def show
+    @player = Player.find(params[:id], :include => :events)
+  end
 end
